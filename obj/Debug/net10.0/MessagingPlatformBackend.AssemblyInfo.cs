495531c72e91a5d761df7791913671553767a5a5
ayushresthaaa <ayushrestha2fb5@gmail.com>
2026-02-08 07:23:15 +0545

    added db context for user, models

--- obj/Debug/net10.0/MessagingPlatformBackend.AssemblyInfo.cs
+++ obj/Debug/net10.0/MessagingPlatformBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MessagingPlatformBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325202490d59fa7b908cb06ec013ee293e70265a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e0a0f47051d25772d5613ded0d83783c00a35b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MessagingPlatformBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MessagingPlatformBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
